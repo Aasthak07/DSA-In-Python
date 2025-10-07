5105d36d3fbdae6cd67a03f93459b64b0cc36303
Aasthak07 <aasthakumari20002@gmail.com>
2025-10-07 17:35:01 +0530

    number

--- striver_pattern_sheet.py
+++ striver_pattern_sheet.py
@@ -27,12 +27,12 @@ for i in range(n3, 0, -1):
 
 
 # # number pattern
-# n4= int(input("enter the number"))
-# for i in range(1,n4+1):
-#     for j in range(1, i+1):
-#         print(j, end="")
-#         # print(i, end="")
-#     print()       
+n4= int(input("enter the number"))
+for i in range(1,n4+1):
+    for j in range(1, i+1):
+        print(j, end="")
+        # print(i, end="")
+    print()       
 
 
 # # equilateral triangle star pattern
