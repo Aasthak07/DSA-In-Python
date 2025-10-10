b97b2367a7cd96472cf77332a8b9636783b4bc85
Aasthak07 <aasthakumari20002@gmail.com>
2025-10-10 15:14:23 +0530

    p5

---
+++ star problem/Reverse_hill.py
@@ -0,0 +1,12 @@
+#increasing space and decreasing star and decreasing star
+
+n=5
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end=" ")
+    for j in range(i,n):
+        print("*", end=" ")
+
+    for j in range(i,n-1):
+        print("*", end=" ")    
+    print()    
---
+++ star problem/hill_pattern.py
@@ -0,0 +1,12 @@
+# Decreasing spaces and Increasing stars and Increasing stars
+
+n=5
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end=" ")
+    for j in range(i):
+        print("*", end=" ")
+
+    for j in range( i+1):
+        print("*", end=" ")
+    print()        
