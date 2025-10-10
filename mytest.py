b1bff21776251bca898af8ff6940ca188ffdba22
Aasthak07 <aasthakumari20002@gmail.com>
2025-10-10 17:15:18 +0530

    dp patterns

--- mytest.py
+++ mytest.py
@@ -1,39 +1,13 @@
-# @
-# @
-# @****      @
-# @   ****   @
-# @      ****@
-#            @
-#            @
 
+n=5
 
-
-
-
-n=4
-
-for i in range(n+1):
-    for j in range(1):
-        print("@", end="")
-    
-
+for i in range(1):
+    for j in range(n-1):
+        print('e', end='')
     for j in range(n):
-        if i==n-2 or j==n :
-            print("*", end="")
-        # else:
-        #     print(" ", end="")
-        
+        print(' ', end='')
+    for j in range(1):
+        print('*', end='') 
 
-    # for j in range(n-3):
-    #     if i==n-1:
-    #         print("*", end="")
 
-    # for j in range(n-1):
-    #     if i ==n-1:
-    #         print("*", end="")
-
-    # for j in range(n-3):
-    #     if i==n :
-    #         print("*", end="")
-
-    print()          
+    print()           
---
+++ wkl12.py
@@ -0,0 +1,35 @@
+#               *
+#             * * *
+#           * * * * *
+#           *       *
+#           *       *
+#           *       *
+# * * * * *           * * * * *
+#   * * *               * * *
+#     *                   *
+
+
+# n=int(input("Enter the value of n: "))
+n=5
+for i in range((n//2)+1):
+    for j in range(n+(n//2)-i):
+        print(" ",end=" ")
+    for i in range(2*i+1):
+        print("*",end=" ")
+    print()
+for i in range((n//2)+1):
+    for j in range(n):
+        print(" ",end=" ")
+    for j in range(n):
+        if(j==0 or j==n-1):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
+for i in range((n//2)+1):
+    for j in range(3*n):
+        if(j>=i and j<=n-1-i) or (j>=2*n+i and j<=3*n-1-i):
+            print("*",end=" ")
+        else:
+            print(" ",end=" ")
+    print()
