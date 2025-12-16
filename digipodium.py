adb3df6839b1b441845d8d9497bed152ea8a5b03
Aasthak07 <aasthakumari20002@gmail.com>
2025-12-16 23:00:08 +0530

    lec2,3

---
+++ digipodium.py
@@ -0,0 +1,5 @@
+#wap to print even no. between 1 to 50
+
+for i in range(1,51):
+    if i%2==0:
+        print(i)    
--- recursion_Time&spaceComplexity.py
+++ recursion_Time&spaceComplexity.py
@@ -10,3 +10,26 @@
 # 3. O(n) - linear time complexity
 # 4. O(n^2) - quadratic time complexity
 # 5. O(2^n) - exponential time complexity
+
+#linear time complexity O(n)
+n=10
+for i in range(n):
+    print(i, end= " " )
+
+# n ke saath koi constant no. se operation ho rha hoga to  order of n hi hoga
+
+# constant time complexity O(1)
+for i in range(10000):
+    print(i, end= " " )
+
+#QUADRATIC TIME COMPLEXITY O(n^2)
+
+for i in range(n**2):
+    print(i, end= " " )
+
+#logarithmic time complexity O(log n)
+n=100
+while n>1:
+    n= n//2
+    print(n)    
+
